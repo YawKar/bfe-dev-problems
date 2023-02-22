2f153ec407a47d53803b63107faba9cad6e753c3
yawkar <yawkarpov@gmail.com>
2023-02-22 11:07:16 +0300

    Add solution to the 8th problem

---
+++ 8/README.md
@@ -0,0 +1,1 @@
+Link to the problem: https://bigfrontend.dev/problem/can-you-shuffle-an-array
---
+++ 8/solution.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * Fisher-Yates shuffle algorithm
+ * @param {any[]} arr
+ * @returns {void}
+ */
+function shuffle(arr) {
+    for (let i = arr.length - 1; i > -1; --i) {
+        let randIndx = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[randIndx]] = [arr[randIndx], arr[i]];
+    }
+}
