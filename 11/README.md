d530a8177682a8fe41bcb78cd722d38d457bca04
yawkar <yawkarpov@gmail.com>
2023-02-23 14:16:49 +0300

    Add solution to the 11th problem

---
+++ 11/README.md
@@ -0,0 +1,1 @@
+Link to the problem: https://bigfrontend.dev/problem/what-is-composition-create-a-pipe
---
+++ 11/solution.js
@@ -0,0 +1,7 @@
+"use strict";
+
+/**
+ * @param {Array<(arg: any) => any>} funcs 
+ * @return {(arg: any) => any}
+ */
+const pipe = funcs => arg => funcs.reduce((val, fn) => fn(val), arg);
