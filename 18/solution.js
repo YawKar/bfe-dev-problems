8bdd8fb8fb4e34f4102d7a3807aecc797ffa21fe
yawkar <yawkarpov@gmail.com>
2023-02-26 15:22:54 +0300

    Add solution to the 18th problem

---
+++ 18/solution.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * @param {object[]} items
+ * @excludes { Array< {k: string, v: any} >} excludes
+ */
+
+/**
+ * @param {object[]} items
+ * @param { Array< {k: string, v: any} >} excludes
+ * @return {object[]}
+ */
+function excludeItems(items, excludes) {
+    return items.filter(item => {
+        return excludes.every(ex => item[ex.k] !== ex.v);
+    });
+}
