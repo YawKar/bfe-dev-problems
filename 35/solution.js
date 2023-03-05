cd771cae00c1e5cd502fd153f4719815e6d0df73
yawkar <yawkarpov@gmail.com>
2023-03-05 12:58:56 +0300

    Add solution to the 35th problem

---
+++ 35/solution.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * @param {Array<Promise>} promises
+ * @return {Promise}
+ */
+function race(promises) {
+  return new Promise((resolve, reject) => {
+    promises.forEach(promise => {
+      promise.then(val => resolve(val), error => reject(error));
+    });
+  });
+}
